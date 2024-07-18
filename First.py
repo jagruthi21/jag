95b84ebe0724c8087d4146bb07816915f06b9b62
jagruthi21 <50977100+jagruthi21@users.noreply.github.com>
2024-07-18 16:35:18 +0530

    Add files via upload

---
+++ First.py
@@ -0,0 +1,9 @@
+a = input('Enter first number')
+print(type(a))
+b = input('Enter second number')
+print(type(b))
+
+if(int(a) > int(b)):
+    print(a, 'Largest', type(a))
+else:
+    print(b, 'Largest', type(b))
